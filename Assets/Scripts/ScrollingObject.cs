cea96708b338bf1e57c67a3be8de41206c0f7a15
qkrwjdtn1236 <48826426+qkrwjdtn1236@users.noreply.github.com>
2021-04-09 11:42:52 +0900

    add Background Loop, Player Controller

---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    public float speed = 10f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Vector3.left);
+        transform.Translate(Vector3.left * speed * Time.deltaTime);    
+    }
+}
